5bebb78451c21782e7e5cf2673bdfa7cf0b4ac92
alfecaesar <alfecaesar@gmail.com>
2024-05-22 01:47:35 +0800

    mobile layout changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,23 @@ var app = {
   init: function() {
     console.log('🟢 Rockville Baptist Church ⛪');
   },
+  mobileNav: function(){
+    var openNav = document.querySelector('.navbar-toggler');
+    var closeNav = document.querySelector('.mob-menu-close');
+    var mobileMenuContainer = document.querySelector('.mobile-menu-container');
+
+    openNav.addEventListener('click', function(){
+      mobileMenuContainer.classList.add('show');
+    });
+    closeNav.addEventListener('click', function(){
+      mobileMenuContainer.classList.remove('show');
+    });
+  },
   scrollTop: function() {
     window.scrollTo({top: 0, behavior: 'smooth'});
   }
 };
 (function() {
   app.init();
+  app.mobileNav();
 })();
